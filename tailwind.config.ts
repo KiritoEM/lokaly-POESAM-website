8b36f0b9d3ece78a1451229d7add5ff3eefda562
kiritoEM <johankirito64@gmail.com>
2024-05-09 09:39:30 +0200

    config colors for tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        green01: "#49CB00",
+        grenn02: "#02260B",
+        gray01: "#B3B3B3",
+        gray02: "#2E3230",
+      },
     },
   },
   plugins: [],
