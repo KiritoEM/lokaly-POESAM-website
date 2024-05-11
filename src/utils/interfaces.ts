27e12553973938b6c9d378dcc3f68359a1db6612
kiritoEM <johankirito64@gmail.com>
2024-05-11 18:23:34 +0200

    add nav-hook

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -9,4 +9,28 @@ interface InavList {
   scrollSectionId?: string;
 }
 
-export type { Ichildren, InavList };
+interface INavProvider {
+  children?: ReactNode;
+}
+
+interface INavContext {
+  openMenu: boolean;
+  openOverlay: boolean;
+  menuToogle: () => void;
+  changeNav: (state: boolean) => void;
+}
+
+interface IDefaultNavContext {
+  openMenu: false;
+  openOverlay: false;
+  menuToogle: () => null;
+  changeNav: (state: boolean) => null;
+}
+
+export type {
+  Ichildren,
+  InavList,
+  IDefaultNavContext,
+  INavContext,
+  INavProvider,
+};
