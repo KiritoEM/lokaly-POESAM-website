1826398beb04c40497ce70abd516572733277978
kiritoEM <johankirito64@gmail.com>
2024-05-11 09:54:43 +0200

    add brand content

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,6 +1,30 @@
+import { useEffect } from "react";
+import { delay, motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 const Brand = (): JSX.Element => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  const variants = {
+    hidden: { opacity: 0, y: 20, x: 35, scale: 0 },
+    visible: { opacity: 1, y: 0, x: 0, scale: 1, transition: { delay: 0.5 } },
+  };
+
   return (
-    <section className="brand bg-green02 px-32 py-10 flex flex-wrap items-center justify-between">
+    <motion.section
+      ref={ref}
+      initial="hidden"
+      animate={controls}
+      variants={variants}
+      className="brand bg-green02 px-32 py-10 flex flex-wrap items-center justify-between"
+    >
       <div className="partenary flex gap-4 items-center">
         <h4 className="text-6xl text-white calSans pt-2">17</h4>
         <p className="text-white01">
@@ -23,13 +47,14 @@ const Brand = (): JSX.Element => {
         </p>
       </div>
       <div className="selling flex gap-4 items-center">
-        <h4 className="text-6xl text-white calSans pt-2">600</h4>
+        <h4 className="text-6xl text-white calSans pt-2">697</h4>
         <p className="text-white01">
-          ventes<br />
+          ventes
+          <br />
           réalisés
         </p>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
