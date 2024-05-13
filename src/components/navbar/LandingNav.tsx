9959083084108c4a4288178926db6d74fa0c2374
kiritoEM <johankirito64@gmail.com>
2024-05-13 21:34:02 +0200

    fix nav

--- src/components/navbar/LandingNav.tsx
+++ src/components/navbar/LandingNav.tsx
@@ -9,61 +9,14 @@ const LandingNav = (): JSX.Element => {
   const { menuToogle, changeNav } = useNav();
   return (
     <Fragment>
-      <motion.nav
-        initial={{ opacity: 0, y: -37 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 5.3 }}
-        className="landing-nav absolute w-full top-0 z-50  bg-transparent overflow-hidden hidden lg:inline"
-      >
-        <div className="nav-container flex justify-between py-10 px-16  bg-transparent">
-          {/* logo */}
-          <div className="logo">
-            <img src="/logo.svg" alt="" />
-          </div>
-
-          {/* menu-items */}
-          <div className="menu-items  gap-10 items-center hidden lg:flex">
-            <ul className="flex gap-10">
-              {navList.map((item, index) => (
-                <li
-                  key={index}
-                  className="general-sans-medium  text-green02 cursor-pointer"
-                  onClick={() => {
-                    scrollSection(
-                      item.scrollSectionId ? item.scrollSectionId : ""
-                    );
-                    changeNav(false);
-                  }}
-                >
-                  {item.label}
-                </li>
-              ))}
-            </ul>
-            <div className="contact-btn">
-              <button className="bg-green02 rounded-3xl py-3 text-white px-6 general-sans-medium">
-                Nous contacter
-              </button>
-            </div>
-          </div>
-
-          {/* menu icon */}
-          <img
-            src="/icons/menu-icon.png"
-            alt=""
-            className="md:hidden w-8 h-8 relative top-1"
-            onClick={() => menuToogle()}
-          />
-        </div>
-      </motion.nav>
-
       {/* navbar  container responsive */}
       <motion.nav
         initial={{ opacity: 0, y: -37 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.5 }}
-        className="landing-nav fixed w-full top-0 z-50  bg-white overflow-hidden lg:hidden"
+        className="landing-nav fixed lg:absolute w-full top-0 z-50  bg-white lg:bg-transparent overflow-hidden"
       >
-        <div className="nav-responsive-container flex justify-between px-10 py-6  bg-transparent">
+        <div className="nav-responsive-container flex justify-between px-10 lg:px-16 bg-transparent py-6  bg-transparent">
           {/* logo */}
           <div className="logo">
             <img src="/logo.svg" alt="" />
