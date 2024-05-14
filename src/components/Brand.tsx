31e1035f01aa93d776602d42aacc6f3f01c2567b
kiritoEM <johankirito64@gmail.com>
2024-05-14 06:26:29 +0200

    add auto-incrementation to brand

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -17,7 +17,7 @@ const Brand = (): JSX.Element => {
             prevCount < overviewData[index] ? prevCount + 1 : prevCount
           )
         );
-      }, 100);
+      }, 40);
 
       return () => clearInterval(interval);
     }
