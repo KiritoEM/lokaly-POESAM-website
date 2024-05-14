1c3a0a135012ba10f8e1bca78ef8eb3dea42f093
kiritoEM <johankirito64@gmail.com>
2024-05-14 06:25:15 +0200

    add auto-incrementation to brand

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,31 +1,70 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 const Brand = (): JSX.Element => {
+  const overviewData = [17, 60, 60, 69];
+  const [counts, setCounts] = useState([0, 0, 0, 0]);
+  const [shouldIncrement, setShouldIncrement] = useState(true);
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (inView && shouldIncrement) {
+      const interval = setInterval(() => {
+        setCounts((prevCounts) =>
+          prevCounts.map((prevCount, index) =>
+            prevCount < overviewData[index] ? prevCount + 1 : prevCount
+          )
+        );
+      }, 100);
+
+      return () => clearInterval(interval);
+    }
+  }, [inView, shouldIncrement]);
+
+  useEffect(() => {
+    if (inView && !shouldIncrement) {
+      setShouldIncrement(true);
+    }
+  }, [inView]);
+
   return (
-    <section className="brand bg-green02 px-10 md:px-32 py-10 flex flex-wrap gap-8 lg:gap-0 items-center justify-between">
+    <section
+      className="brand bg-green02 px-10 md:px-32 py-10 flex flex-wrap gap-8 lg:gap-0 items-center justify-between"
+      ref={ref}
+    >
       <div className="partenary flex  gap-5 md:gap-4 items-center">
-        <h4 className="text-5xl lg:text-6xl text-white calSans pt-2">17</h4>
+        <h4 className="text-5xl lg:text-6xl text-white calSans pt-2">
+          {counts[0]}
+        </h4>
         <p className="text-white01">
           Partenaires <br />
           locaux
         </p>
       </div>
       <div className="job flex gap-5 md:gap-4 items-center">
-        <h4 className="text-5xl lg:text-6xl text-white calSans pt-2">+60</h4>
+        <h4 className="text-5xl lg:text-6xl text-white calSans pt-2">
+          +{counts[1]}
+        </h4>
         <p className="text-white01">
           Emplois <br />
           crées
         </p>
       </div>
       <div className="food flex gap-5 md:gap-4 items-center">
-        <h4 className="text-5xl lg:text-6xl text-white calSans pt-2">600</h4>
+        <h4 className="text-5xl lg:text-6xl text-white calSans pt-2">
+          {counts[2]}
+        </h4>
         <p className="text-white01">
           Kilos de <br />
           nourriture sauvé
         </p>
       </div>
       <div className="selling flex gap-5 md:gap-4 items-center">
-        <h4 className="text-5xl lg:text-6xl text-white calSans pt-2">697</h4>
+        <h4 className="text-5xl lg:text-6xl text-white calSans pt-2">
+          {counts[3]} {/* Utiliser counts[3] pour les ventes */}
+        </h4>
         <p className="text-white01">
           ventes
           <br />
