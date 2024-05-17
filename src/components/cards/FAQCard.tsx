bc056745e5f364ad864ec4b2b55d883b0ef0bf9c
kiritoEM <johankirito64@gmail.com>
2024-05-17 09:28:44 +0200

    add FAQ-card responsive

--- src/components/cards/FAQCard.tsx
+++ src/components/cards/FAQCard.tsx
@@ -19,17 +19,20 @@ const FAQCard = ({ text, title, isActive, handleToggle }: FAQCardProps): JSX.Ele
 
     return (
         <article
-            className={`FAQ-card flex w-full rounded-xl cursor-pointer ${isActive ? "sticky-open" : "sticky-close"}  ${isActive ? "border-2 border-green01 items-start gap-12 p-10" : "items-center justify-between  py-6 px-10"}`}
+            className={`FAQ-card flex w-full rounded-xl cursor-pointer ${isActive ? "sticky-open" : "sticky-close"}  ${isActive ? "border-2 border-green01 items-start gap-12 p-10" : "items-center justify-between  py-5 md:py-6 px-8 md:px-10"}`}
             onClick={() => { handleToggle(); handleImageClick() }}
         >
-            <div className={`header ${isActive && "flex flex-col gap-5"}`}>
-                <h3 className="text-2xl text-blue01 calSans">{title}</h3>
+            <div className={`header w-11/12	md:w-full ${isActive && "flex flex-col gap-5"}`}>
+                <h3 className="text-xl md:text-2xl text-blue01 calSans">{title}</h3>
                 <p className={`text-blue02  ${isActive ? "flex" : "hidden"}`}>{text}</p>
             </div>
             <img
                 src={isActive ? "/icons/chevron-circle-down.svg" : "/icons/chevron-circle-right.svg"}
-                className={`cursor-pointer ${isRotating && "rotate"}`}
-            // onClick={handleImageClick}
+                className={`w-12 md:w-auto cursor-pointer absolute right-0  md:relative hidden md:flex ${isRotating && "rotate"}`}
+            />
+            <img
+                src="/icons/chevron-circle-right.svg"
+                className={`w-12 md:w-auto cursor-pointer absolute right-0 mr-12 md:relative md:hidden ${isActive && "md:hidden"}  ${isActive && "rotate-respons"}`}
             />
         </article>
     );
