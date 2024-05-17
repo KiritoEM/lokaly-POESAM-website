5ac4be58117a76433c43140c971e81bb2045a0df
kiritoEM <johankirito64@gmail.com>
2024-05-17 20:29:01 +0200

    add data to FFAQ

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -32,12 +32,12 @@ const FAQData: IFAQCard[] = [
   },
   {
     title: "Quels types de produits puis-je trouver sur Lokaly ?",
-    text: "Lokaly connecte les producteurs, commerçants et consommateurs via une plateforme digitale. Les producteurs inscrivent leurs produits, les commerçants peuvent gérer leurs stocks et les consommateurs peuvent acheter des produits locaux frais, réduisant ainsi le gaspillage alimentaire et soutenant l'économie locale.",
+    text: "Sur Lokaly, vous pouvez trouver une variété de produits locaux, notamment des produits agricoles frais tels que des fruits et légumes, des produits issus des food businesses pour le programme NoWaste, et des alternatives durables comme les charbons éco et le biogaz.",
   },
   {
     title:
       "Comment Lokaly contribue-t-elle à la réduction du gaspillage alimentaire ?",
-    text: "Lokaly connecte les producteurs, commerçants et consommateurs via une plateforme digitale. Les producteurs inscrivent leurs produits, les commerçants peuvent gérer leurs stocks et les consommateurs peuvent acheter des produits locaux frais, réduisant ainsi le gaspillage alimentaire et soutenant l'économie locale.",
+    text: "Lokaly aide à réduire le gaspillage alimentaire en permettant aux producteurs et commerçants de vendre leurs produits invendus ou en surplus à des consommateurs à la recherche de produits frais et locaux. Cela valorise les excédents et évite que des produits parfaitement bons ne soient jetés, contribuant ainsi à une économie circulaire et durable.",
   },
 ];
 
