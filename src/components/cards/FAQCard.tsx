6214da2622861a138dc7b5774790c30ed29ba161
kiritoEM <johankirito64@gmail.com>
2024-05-17 05:40:44 +0200

    add FAQ-cardsticky-animation

--- src/components/cards/FAQCard.tsx
+++ src/components/cards/FAQCard.tsx
@@ -1,4 +1,5 @@
 import { IFAQCard } from "@/utils/interfaces";
+import { useState } from "react";
 
 interface FAQCardProps extends IFAQCard {
     isActive: boolean;
@@ -6,14 +7,30 @@ interface FAQCardProps extends IFAQCard {
 }
 
 const FAQCard = ({ text, title, isActive, handleToggle }: FAQCardProps): JSX.Element => {
+    const [isRotating, setIsRotating] = useState<boolean>(false);
+
+    //fonction pour la rotation de l' icone
+    const handleImageClick = () => {
+        setTimeout(() => {
+            setIsRotating(true);
+            console.log("rotating")
+        }, 100)
+    }
+
     return (
-        <article className={`FAQ-card flex w-full rounded-xl ${isActive ? "border-2 border-green01 items-center gap-12 p-10" : "items-center justify-between py-6 px-10"}`}>
-            <div className="header flex flex-col gap-5">
+        <article
+            className={`FAQ-card flex w-full rounded-xl cursor-pointer ${isActive ? "sticky-open" : "sticky-close"}  ${isActive ? "border-2 border-green01 items-start gap-12 p-10" : "items-center justify-between  py-6 px-10"}`}
+            onClick={() => { handleToggle(); handleImageClick() }}
+        >
+            <div className={`header ${isActive && "flex flex-col gap-5"}`}>
                 <h3 className="text-2xl text-blue01 calSans">{title}</h3>
-                <p className={`text-blue02 ${isActive ? "flex" : "hidden"}`}>{text}</p>
+                <p className={`text-blue02  ${isActive ? "flex" : "hidden"}`}>{text}</p>
             </div>
-            {isActive ?
-                (<img src="/icons/chevron-circle-down.svg" className="cursor-pointer" onClick={handleToggle} />) : (<img src="/icons/chevron-circle-right.svg" className="cursor-pointer" onClick={handleToggle} />)}
+            <img
+                src={isActive ? "/icons/chevron-circle-down.svg" : "/icons/chevron-circle-right.svg"}
+                className={`cursor-pointer ${isRotating && "rotate"}`}
+            // onClick={handleImageClick}
+            />
         </article>
     );
 };
