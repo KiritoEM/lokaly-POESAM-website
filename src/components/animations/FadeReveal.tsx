825c613955174c09266f19d40f014a9016141c1b
kiritoEM <johankirito64@gmail.com>
2024-05-18 08:32:45 +0200

    add contact with next/api

--- src/components/animations/FadeReveal.tsx
+++ src/components/animations/FadeReveal.tsx
@@ -13,7 +13,7 @@ const FadeReveal = ({ children }: Ichildren): JSX.Element => {
       <motion.div
         initial={{ opacity: 0, y: 70 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.5, duration: 0.6 }}
+        transition={{ delay: 0.54, duration: 0.7 }}
       >
         {children}
       </motion.div>
