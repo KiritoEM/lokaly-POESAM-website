e964b6c356710e9914a97de285e746da8de9779a
kiritoEM <johankirito64@gmail.com>
2024-06-02 17:10:38 +0200

    add icon for meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <meta name="description" content="Loka" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo-icon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
